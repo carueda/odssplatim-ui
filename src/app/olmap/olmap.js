9c8fc6fbe7adf49c6845b7e01c7c51b63d050d7e
Carlos Rueda <carueda@mbari.org>
2015-04-09 15:27:44 -0700

    initial geometry editing with openlayers3

--- src/app/olmap/olmap.js
+++ src/app/olmap/olmap.js
@@ -6,10 +6,32 @@ angular.module('odssPlatimApp.olmap', ['odssPlatimApp.olmap.directives'])
     .factory('olmap', olmapFactory)
 ;
 
-MapCtrl.$inject = ['olmap'];
+MapCtrl.$inject = ['$scope', 'olmap'];
 
-function MapCtrl(olmap) {
+function MapCtrl($scope, olmap) {
     olmap.insertMap();
+
+    $scope.mv = {
+        editMode: false,
+        draw: {
+            typeList: [
+                {name: "Polygon", value: ol.geom.GeometryType.POLYGON},
+                {name: "LineString", value: ol.geom.GeometryType.LINE_STRING},
+                {name: "Point", value: ol.geom.GeometryType.POINT},
+                {name: "(no)", value: ""}
+            ],
+            selectedType: ""
+        }
+    };
+    $scope.$watch('mv.draw.selectedType', function(type) {
+        olmap.setDrawType(type);
+    });
+    $scope.$watch('mv.editMode', function(editMode) {
+        olmap.setEditMode(editMode);
+        if (editMode) {
+            olmap.setDrawType($scope.mv.draw.selectedType);
+        }
+    });
 }
 
 olmapFactory.$inject = [];
@@ -19,16 +41,19 @@ function olmapFactory() {
     var gmap = createGMap();
     var view = createView();
     var layers = createLayers();
+    var map, draw, modify, featureOverlay;
 
     return {
         insertMap:   insertMap
         ,setCenter:  setCenter
         ,setZoom:    setZoom
+        ,setEditMode:  setEditMode
+        ,setDrawType:  setDrawType
     };
 
     function insertMap() {
         var olMapDiv = document.getElementById('omap2');
-        var map = new ol.Map({
+        map = new ol.Map({
             layers: layers,
             interactions: ol.interaction.defaults({
                 altShiftDragRotate: false,
@@ -53,6 +78,8 @@ function olmapFactory() {
 
         olMapDiv.parentNode.removeChild(olMapDiv);
         gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(olMapDiv);
+
+        prepareEditing();
     }
 
     function setCenter(ll) {
@@ -86,18 +113,87 @@ function olmapFactory() {
     }
 
     function createLayers() {
-        var vector = new ol.layer.Vector({
-            source: new ol.source.GeoJSON({
-                url: 'olmap/data/countries.geojson',
-                projection: 'EPSG:3857'
-            }),
+        return [];
+        //var vector = new ol.layer.Vector({
+        //    source: new ol.source.GeoJSON({
+        //        url: 'olmap/data/countries.geojson',
+        //        projection: 'EPSG:3857'
+        //    }),
+        //    style: new ol.style.Style({
+        //        //fill: new ol.style.Fill({ color: 'rgba(255, 255, 255, 0.6)'}),
+        //        stroke: new ol.style.Stroke({ color: '#319FD3', width: 1})
+        //    })
+        //});
+        //return [vector]
+    }
+
+    function prepareEditing() {
+        // The features are not added to a regular vector layer/source,
+        // but to a feature overlay which holds a collection of features.
+        // This collection is passed to the modify and also the draw
+        // interaction, so that both can add or modify features.
+        featureOverlay = new ol.FeatureOverlay({
             style: new ol.style.Style({
-                //fill: new ol.style.Fill({ color: 'rgba(255, 255, 255, 0.6)'}),
-                stroke: new ol.style.Stroke({ color: '#319FD3', width: 1})
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 255, 255, 0.2)'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: '#ffcc33',
+                    width: 2
+                }),
+                image: new ol.style.Circle({
+                    radius: 7,
+                    fill: new ol.style.Fill({
+                        color: '#ffcc33'
+                    })
+                })
             })
         });
-        return [vector]
+        featureOverlay.setMap(map);
+
     }
+
+    function setEditMode(editMode) {
+        if (modify) {
+            map.removeInteraction(modify);
+            modify = null;
+        }
+        if (editMode) {
+            modify = new ol.interaction.Modify({
+                features: featureOverlay.getFeatures(),
+                // the SHIFT key must be pressed to delete vertices, so
+                // that new vertices can be drawn at the same position
+                // of existing vertices
+                deleteCondition: function(event) {
+                    return ol.events.condition.shiftKeyOnly(event) &&
+                        ol.events.condition.singleClick(event);
+                }
+            });
+            map.addInteraction(modify);
+        }
+        else {
+            setDrawType(null); // no draw type
+        }
+    }
+
+    /**
+     * @param type {ol.geom.GeometryType} or falsy to disable
+     */
+    function setDrawType(type) {
+        //console.log("setDrawType=", type);
+        if (draw) {
+            map.removeInteraction(draw);
+            draw = null;
+        }
+        if (type) {
+            draw = new ol.interaction.Draw({
+                features: featureOverlay.getFeatures(),
+                type: type
+            });
+            map.addInteraction(draw);
+        }
+    }
+
 }
 
 })();
